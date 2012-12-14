3df962c5d2b9f8ffd502004ada1d29edb69da172
Marek Goldmann <marek.goldmann@gmail.com>
2012-12-14 11:49:41 +0100

    Added default root to main page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Poorsmatic::Application.routes.draw do
+  get "root/index"
+
   resources :term_counts
 
 
@@ -64,4 +66,6 @@ Poorsmatic::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  root :to => 'root#index'
 end
